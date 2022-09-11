ef5581d490c7e39cbbec42760b4688463ab2229b
Riley Burns <rileyburns345@gmail.com>
2022-09-11 14:16:50 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class('example1') ?>>
+    <?php wp_body_open(); ?>
+    <p>Hello World YAYAYAY</p>
+    <?php wp_footer(); ?>
+</body>
+</html>
